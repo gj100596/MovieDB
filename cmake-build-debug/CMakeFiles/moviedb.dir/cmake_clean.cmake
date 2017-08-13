b20ebdd93ebb030803d323b75183040f925a251c
sapan <sapan@cse.iitb.ac.in>
2017-08-13 16:19:53 +0530

    Starting

---
+++ cmake-build-debug/CMakeFiles/moviedb.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/moviedb.dir/main.cpp.o"
+  "moviedb.pdb"
+  "moviedb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/moviedb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
