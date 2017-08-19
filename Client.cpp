ae4ddd9423ecfc120189f87f2aea9bddd5fcdaa9
gauravj <gauravj@cse.iitb.ac.in>
2017-08-19 15:10:36 +0530

    Final Socket Working

--- Client.cpp
+++ Client.cpp
@@ -4,12 +4,10 @@
 
 #include <netinet/in.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
 #include "iostream"
 #include "arpa/inet.h"
 #include "stdlib.h"
 #include "sys/socket.h"
-#include "Classes.cpp"
 
 using namespace std;
 
@@ -20,31 +18,74 @@ int main(){
     //int clinet_size = sizeof(client_address);
 
     my_socket = socket(AF_INET,SOCK_STREAM,0);
-    ioctl(my_socket, FIONBIO, 0);
 
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(8080);
+    server_address.sin_port = htons(8081);
     inet_aton( "127.0.0.1", &server_address.sin_addr);
 
-    if(connect(my_socket,(sockaddr *) &server_address, sizeof(server_address))<0){
-        cout<<"Not Connected";
-    }
-    cout<<".......connected"<<endl;
+    connect(my_socket,(sockaddr *) &server_address, sizeof(server_address));
+    cout<<"connected"<<endl;
 
-    string* buff;
-    string final_string;
+    char buff[1024];
+    string final;
     int n;
-    cout<<"Collecting Data";
-
-
-    while((n= recv(my_socket,buff,10,0)) > 0) {
-        cout <<"n is ..."<<n<<endl;
-//        movie *obj;
-//        obj = (movie *)&buff;
-//        obj->print_table();
-        string* b = (string *)buff;
-        cout << b;
+    while((n= recv(my_socket,buff,1024,0)) > 0) {
+        cout << buff;
+        //cout << buff;
     }
-    cout << "Data\n"<<final_string;
+    cout << final;
     return 0;
 }
+
+
+
+////
+//// Created by gaurav on 15/8/17.
+////
+//
+//#include <netinet/in.h>
+//#include <unistd.h>
+//#include <sys/ioctl.h>
+//#include "iostream"
+//#include "arpa/inet.h"
+//#include "stdlib.h"
+//#include "sys/socket.h"
+//#include "Classes.cpp"
+//
+//using namespace std;
+//
+//int main(){
+//
+//    int my_socket;
+//    struct sockaddr_in server_address;
+//    //int clinet_size = sizeof(client_address);
+//
+//    my_socket = socket(AF_INET,SOCK_STREAM,0);
+////    ioctl(my_socket, FIONBIO, 0);
+//
+//    server_address.sin_family = AF_INET;
+//    server_address.sin_port = htons(8080);
+//    inet_aton( "127.0.0.1", &server_address.sin_addr);
+//
+//    if(connect(my_socket,(sockaddr *) &server_address, sizeof(server_address))<0){
+//        cout<<"Not Connected";
+//    }
+//    cout<<".......connected"<<endl;
+//
+//    string* buff;
+//    string final_string;
+//    int n;
+//    cout<<"Collecting Data";
+//
+//
+//    while((n= recv(my_socket,buff,10,MSG_WAITALL)) > 0) {
+//        cout <<"n is ..."<<n<<endl;
+////        movie *obj;
+////        obj = (movie *)&buff;
+////        obj->print_table();
+//        string* b = (string *)buff;
+//        cout << b;
+//    }
+//    cout << "Data\n"<<final_string;
+//    return 0;
+//}
--- Server.cpp
+++ Server.cpp
@@ -11,7 +11,6 @@
 #include "mysql_connection.h"
 
 #include "cppconn/statement.h"
-#include "Classes.cpp"
 
 using namespace sql;
 
@@ -33,34 +32,22 @@ void send_movie_list(int client_socket){
 
         res = stmt->executeQuery("Select id,title from movie");
 
-//        send(client_socket,&title,sizeof(title),0);
-
-        string buffer = "ID \t Title\n";
+//        char *title = "ID \t Title\n";
+//        send(client_socket,title,sizeof(title),0);
         while(res->next()){
             ostringstream oss;
-            oss<<res->getInt(1);
-//
-//            movie obj(res->getInt(1),res->getString(2));
-//            obj.print_table();
-//            send(client_socket,&obj,sizeof(obj),0);
-
+            oss << res->getInt(1);
             string entry;
             entry.append(oss.str());
-//            entry.append(std::to_string(res->getInt(1)));
             entry.append("\t");
             entry.append(res->getString(2));
             entry.append("\n");
 
-            int a = send(client_socket,&entry,sizeof(entry),0);
-            cout<<entry<<"................."<<a;
+            char buff[1024];
+            strcpy(buff,entry.c_str());
+            send(client_socket,buff,sizeof(buff),0);
+            cout<< res->getInt(1) << "\t" << res->getString(2) <<endl;
         }
-
-//        char buff[1024];
-//        strcpy(buff,buffer.c_str());
-//        send(client_socket,buff,sizeof(buff),0);
-//        cout<<"Sending...\n"<<buff;
-//        cout<<sizeof(buff);
-//        cout<< res->getInt(1) << "\t" << res->getString(2) <<endl;
 
         delete res;
         delete stmt;
@@ -78,7 +65,7 @@ int main(){
 
     my_address.sin_family = AF_INET;
     my_address.sin_addr.s_addr = INADDR_ANY;
-    my_address.sin_port = htons(8080);
+    my_address.sin_port = htons(8081);
 
     bind(server_socket,(struct sockaddr *)&my_address, sizeof(my_address));
 
@@ -87,7 +74,7 @@ int main(){
 
         cout << "listening"<<endl;
         if((client_socket = accept(server_socket,(struct sockaddr *) &client_address,
-                               (socklen_t *)(&clinet_size))) <0){
+                                   (socklen_t *)(&clinet_size))) <0){
             cout << "Error" <<endl;
         }
 
@@ -99,3 +86,108 @@ int main(){
     }
     return 0;
 }
+
+
+
+
+////
+//// Created by gaurav on 15/8/17.
+////
+//
+//#include <netinet/in.h>
+//#include "iostream"
+//#include "sys/socket.h"
+//#include <mysql_driver.h>
+//#include <cstring>
+//#include <sstream>
+//#include "mysql_connection.h"
+//
+//#include "cppconn/statement.h"
+//#include "Classes.cpp"
+//
+//using namespace sql;
+//
+//using namespace std;
+//
+//void send_movie_list(int client_socket){
+//    sql::Driver *driver;
+//    sql::Connection *con;
+//    sql::Statement *stmt;
+//    sql::ResultSet *res;
+//
+//    driver = get_driver_instance();
+//    con = driver->connect("tcp://127.0.0.1:3306", "root", "0882");
+//
+//    if(con->isValid()) {
+//
+//        stmt = con->createStatement();
+//        stmt->execute("USE moviedb");
+//
+//        res = stmt->executeQuery("Select id,title from movie");
+//
+////        send(client_socket,&title,sizeof(title),0);
+//
+//        string buffer = "ID \t Title\n";
+//        while(res->next()){
+//            ostringstream oss;
+//            oss<<res->getInt(1);
+////
+////            movie obj(res->getInt(1),res->getString(2));
+////            obj.print_table();
+////            send(client_socket,&obj,sizeof(obj),0);
+//
+//            string entry;
+//            entry.append(oss.str());
+////            entry.append(std::to_string(res->getInt(1)));
+//            entry.append("\t");
+//            entry.append(res->getString(2));
+//            entry.append("\n");
+//
+//            int a = send(client_socket,&entry,sizeof(entry),0);
+//            cout<<entry<<"................."<<a;
+//        }
+//
+////        char buff[1024];
+////        strcpy(buff,buffer.c_str());
+////        send(client_socket,buff,sizeof(buff),0);
+////        cout<<"Sending...\n"<<buff;
+////        cout<<sizeof(buff);
+////        cout<< res->getInt(1) << "\t" << res->getString(2) <<endl;
+//
+//        delete res;
+//        delete stmt;
+//        delete con;
+//    }
+//}
+//
+//int main(){
+//
+//    int server_socket,client_socket;
+//    struct sockaddr_in my_address,client_address;
+//    int clinet_size = sizeof(client_address);
+//
+//    server_socket = socket(AF_INET,SOCK_STREAM,0);
+//
+//    my_address.sin_family = AF_INET;
+//    my_address.sin_addr.s_addr = INADDR_ANY;
+//    my_address.sin_port = htons(8080);
+//
+//    bind(server_socket,(struct sockaddr *)&my_address, sizeof(my_address));
+//
+//    while(true){
+//        listen(server_socket,50);
+//
+//        cout << "listening"<<endl;
+//        if((client_socket = accept(server_socket,(struct sockaddr *) &client_address,
+//                               (socklen_t *)(&clinet_size))) <0){
+//            cout << "Error" <<endl;
+//        }
+//
+//        //char* h = "Hello";
+//        cout << client_socket <<endl;
+//        cout<< "Starting Giving Data"<<endl;
+//        send_movie_list(client_socket);
+//
+//    }
+//    return 0;
+//}
