e702c4af0c279b58446b89a5b79349db48d3bcd2
sapan <sapantanted99@gmail.com>
2017-09-01 00:15:55 +0530

    Nothing much changed...just some comments and all

--- Client.cpp
+++ Client.cpp
@@ -3,7 +3,6 @@
 //
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-
 #include <netinet/in.h>
 #include <unistd.h>
 #include <fstream>
@@ -13,12 +12,16 @@
 #include "stdlib.h"
 #include "sys/socket.h"
 
-#define PORTNO 8081
-
-void get_user_choice(int server_socket);
+#define PORTNO 8080
 using namespace cv;
-
 using namespace std;
+
+void get_user_choice(int server_socket) {
+    cout<<"\nEnter Movie ID: \n";
+    char inp[128];
+    cin >> inp;
+    send(server_socket,inp, sizeof(inp),0);
+}
 
 void get_movie_titles(int server_socket){
     char buff[1024];
@@ -31,14 +34,6 @@ void get_movie_titles(int server_socket){
         }
         cout << buff;
     }
-}
-
-void get_user_choice(int server_socket) {
-    cout<<"\nEnter Movie ID: \n";
-    char inp[128];
-    cin >> inp;
-    send(server_socket,inp, sizeof(inp),0);
-
 }
 
 void get_movie_detail(int server_socket){
@@ -57,20 +52,15 @@ void get_movie_detail(int server_socket){
 
 void open_image(char* abs_path){
     Mat image;
-    image = imread(abs_path, CV_LOAD_IMAGE_COLOR);   // Read the file
-
-    if (!image.data)                              // Check for invalid input
-    {
+    image = imread(abs_path, CV_LOAD_IMAGE_COLOR);      // Read the file
+    if (!image.data){                                   // Check for invalid input
         cout << "Could not open or find the image" << std::endl;
         return;
     }
     cout<<"Displaying Poster"<<endl;
-    namedWindow("Display window", WINDOW_AUTOSIZE);// Create a window for display.
-    imshow("Display window", image);                   // Show our image inside it.
-
-    waitKey(0);                                          // Wait for a keystroke in the window
-
-
+    namedWindow("Display window", WINDOW_AUTOSIZE);     // Create a window for display.
+    imshow("Display window", image);                    // Show our image inside it.
+    waitKey(0);                                         // Wait for a keystroke in the window
 }
 
 void get_movie_poster(int server_socket){
@@ -90,7 +80,6 @@ void get_movie_poster(int server_socket){
 
         poster.write(buff, sizeof(buff));
     }
-
     poster.close();
     cout << "\nPoster Downloaded. Press 1 to See! 0 to Exit";
     int option=0;
@@ -101,102 +90,20 @@ void get_movie_poster(int server_socket){
 }
 
 int main(){
-
     int my_socket;
     struct sockaddr_in server_address;
     //int clinet_size = sizeof(client_address);
-
     my_socket = socket(AF_INET,SOCK_STREAM,0);
-
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORTNO);
     inet_aton( "127.0.0.1", &server_address.sin_addr);
-
     connect(my_socket,(sockaddr *) &server_address, sizeof(server_address));
 //    cout<<"connected"<<endl;
-
     get_movie_titles(my_socket);
-//    char buff[1024];
-//    int n;
-//    cout<< "ID\tMovie Title\n";
-//    while((n= recv(my_socket,buff,1024,0)) > 0) {
-//        string s = buff;
-//        if(s.compare("-1")==0){
-//            break;
-//        }
-//        cout << buff;
-//    }
-
     get_user_choice(my_socket);
-//    cout<<"\nEnter Movie ID: \n";
-//    char inp[128];
-//    cin >> inp;
-//    send(my_socket,inp, sizeof(inp),0);
-
     get_movie_detail(my_socket);
-//    cout<< "\n Movie Details: \n";
-//    while((n= recv(my_socket,buff,1024,0)) > 0) {
-//        string s = buff;
-//        if(s.compare("-1")==0){
-//            break;
-//        }
-//        cout << buff;
-//    }
     get_movie_poster(my_socket);
+
 
     return 0;
 }
-
-
-
-
-////
-//// Created by gaurav on 15/8/17.
-////
-//
-//#include <netinet/in.h>
-//#include <unistd.h>
-//#include <sys/ioctl.h>
-//#include "iostream"
-//#include "arpa/inet.h"
-//#include "stdlib.h"
-//#include "sys/socket.h"
-//#include "Classes.cpp"
-//
-//using namespace std;
-//
-//int main(){
-//
-//    int my_socket;
-//    struct sockaddr_in server_address;
-//    //int clinet_size = sizeof(client_address);
-//
-//    my_socket = socket(AF_INET,SOCK_STREAM,0);
-////    ioctl(my_socket, FIONBIO, 0);
-//
-//    server_address.sin_family = AF_INET;
-//    server_address.sin_port = htons(8080);
-//    inet_aton( "127.0.0.1", &server_address.sin_addr);
-//
-//    if(connect(my_socket,(sockaddr *) &server_address, sizeof(server_address))<0){
-//        cout<<"Not Connected";
-//    }
-//    cout<<".......connected"<<endl;
-//
-//    string* buff;
-//    string final_string;
-//    int n;
-//    cout<<"Collecting Data";
-//
-//
-//    while((n= recv(my_socket,buff,10,MSG_WAITALL)) > 0) {
-//        cout <<"n is ..."<<n<<endl;
-////        movie *obj;
-////        obj = (movie *)&buff;
-////        obj->print_table();
-//        string* b = (string *)buff;
-//        cout << b;
-//    }
-//    cout << "Data\n"<<final_string;
-//    return 0;
-//}
--- opencv_install_script.sh
+++ opencv_install_script.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Removing  older versions of OpenCV
 sudo apt-get remove libopencv
 sudo apt-get remove opencv
@@ -15,10 +16,12 @@ sudo git clone https://github.com/Itseez/opencv.git
 sudo git clone https://github.com/Itseez/opencv_contrib.git
 
 #Pre-Build Configuration
+cd /opt
+sudo chmod 777 opencv
 cd /opt/opencv
-mkdir release
+sudo mkdir release
 cd release
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules /opt/opencv/
+sudo cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules /opt/opencv/
 
 #Make and install
 cd /opt/opencv/release
