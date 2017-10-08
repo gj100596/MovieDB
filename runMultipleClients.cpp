f75f0e6e6d9b52f4b4d76d07bb98b6c81ab6a255
gauravj <gauravj@cse.iitb.ac.in>
2017-10-08 19:27:30 +0530

    Comments in run....to be removed

--- runMultipleClients.cpp
+++ runMultipleClients.cpp
@@ -16,8 +16,11 @@ float allowed_time = 60.0;
 void * runCommand(void * id){
     int iid = *((int *)id);
     int no_request = 0;
+    cout << iid << " thread starting"<< endl;
+
     while(true){
         if (difftime(time(NULL),start_time)  > allowed_time){
+            cout << iid << " Breaking "<<endl;
             break;
         }
 
@@ -25,24 +28,30 @@ void * runCommand(void * id){
         client_printless(1,m,'n','n',iid,no_request,8090);
         no_request++;
     }
+    cout << iid << " break done"<<endl;
+
     rtime[iid] = no_request;
+    cout << iid << " thread closed"<<endl;
     pthread_exit(0);
 }
 
 float do_work(int no_of_clients){
     pthread_t thread[no_of_clients];
+    cout << "Starting\n";
     for (int i = 0; i < no_of_clients; ++i) {
         int* id=(int *) malloc(1* sizeof(int));
         *id=i;
         pthread_create(&thread[i], NULL, runCommand, (void *) id);
     }
     for (int i = 0; i < no_of_clients; ++i) {
-        pthread_join(thread[i], NULL);
+        cout << pthread_join(thread[i], NULL);
+        cout << i << "... thread joined" <<endl;
     }
+    cout << "Join Done" << endl;
     float total_time = 0;
     for (int i = 0; i < no_of_clients; ++i)
         total_time+=rtime[i];
-
+    cout << "Sum Calculated" << endl;
     return total_time/allowed_time;
 
 }
